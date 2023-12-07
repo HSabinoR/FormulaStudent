24e9de524ae49eba49df3a7742181f00f37717dc
HSabinoR <153095110+HSabinoR@users.noreply.github.com>
2023-12-07 09:43:41 +0000

    Added GUI test code

---
+++ GUI Code/Serial_Send.ino
@@ -0,0 +1,17 @@
+
+const int sensorPin;
+void setup() {
+    Serial.begin(9600);
+}
+
+void loop() {
+    int sensorValue = analogRead(sensorPin);
+    int testValue = 25;
+
+    sensorValue = map(sensorValue, 0, 1023, -180, 180);
+
+    Serial.print(sensorValue);
+    Serial.print(",");
+    Serial.println(testValue);
+    delay(50);
+}
